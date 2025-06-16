816cd42960a6944d27b48c5f4086fea7e60b9a7e
Code-Turbine <codeturbine@gmail.com>
2025-06-16 10:30:07 -0600

    Código de Hola Mundo en Python!!
    Código de Hola Mundo en Python!!

---
+++ holamundo.py
@@ -0,0 +1,20 @@
+'''
+Codigo Creado por Code Turbine.
+
+https://www.youtube.com/@CodeTurbine
+'''
+
+'''Este código de "Hola, Mundo" en Python es muy simple 
+y se utiliza para demostrar el funcionamiento 
+básico del lenguaje'''
+
+"""En esencia, el siguiente codigo imprimira 
+el texto "Hola, Mundo!" en la consola"""
+
+#Este es un print con un string entre comillas dobles
+print("Hola Mundo")
+
+#Este es un print con un string entre comillas simples
+print('Hola Mundo')
+
+print("Hola", 'Mundo')
